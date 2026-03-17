17b0a14063a234e941cf49d9c5158edf28045aaa
Ntsako Ngobeni <ngobeni2302@gmail.com>
2026-03-17 20:35:19 +0200

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,30 @@
+def separate_scores(scores):
+    pass
+
+
+def split_coordinates(coords):
+    pass
+
+
+def build_student_index(students):
+    pass
+
+
+def map_items_to_position(items):
+    pass
+
+
+def normalize_tags(tags):
+    pass
+
+
+def clean_usernames(usernames):
+    pass
+
+
+def group_by_city(people):
+    pass
+
+
+def categorize_books(books):
+    pass
